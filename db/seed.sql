d9ec91b1ba286b8f31eee09a3b2bc553d0ca8020
George Andrew Phineas Coon <phineas@Georges-MacBook-Pro.local>
2020-03-03 11:48:04 -0700

    another commit

---
+++ db/seed.sql
@@ -0,0 +1,16 @@
+CREATE TABLE ppusers (
+    user_id SERIAL PRIMARY KEY,
+    email VARCHAR(200),
+    password VARCHAR(200),
+    first_name VARCHAR(50),
+    last_name VARCHAR(50),
+    profile_img VARCHAR(200)
+);
+
+CREATE TABLE ppposts (
+    post_id SERIAL PRIMARY KEY,
+    user_id INT REFERENCES ppusers(user_id),
+    post_content VARCHAR(500)
+
+);
+
